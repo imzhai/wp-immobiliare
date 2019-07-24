c03208cefad75204cc8fa8c06abdc19bc6319e12
imzhai <jue.yguel@gmail.com>
2019-07-24 11:28:04 +0200

    -

--- index.php
+++ index.php
@@ -5,5 +5,17 @@
             <h1 class="text-center my-4">Bienvenue sur <?php bloginfo('name'); ?></h1>
             <p><?php bloginfo('description'); ?></p>
       
+    <?php 
+    
+        if(have_posts()){ // si on a des articles
+            while (have_posts()){  the_post();// On parcourt les carticles ?>
+                <h1><?php the_title(); ?> </h1>
+              
+           
+           <?php }
+        }
+    
+    ?>
+
 
 <?php get_footer() ?>
