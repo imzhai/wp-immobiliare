db46ec434e27b19765b073f8f4bec2effae2a7f3
imzhai <jue.yguel@gmail.com>
2019-07-24 10:35:46 +0200

    charge header footer functions

--- header.php
+++ header.php
@@ -3,12 +3,15 @@
     <head>
         <meta charset="utf-8">
         <title>Mon thème WordPress</title>
-        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+        <!-- Permet de charger les styles CSS de WP-->
+        <?php wp_head(); ?>
     </head>
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
                 <a class="navbar-brand" href="#"><?php bloginfo('name') ?> </a>
+                <title><?php wp_title('|'); ?></title>
+             
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
