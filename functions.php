5d03e83f306d35e48865cd633bde657bc64f95cd
imzhai <jue.yguel@gmail.com>
2019-07-25 16:42:36 +0200

    crea bdd

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ function immobiliare_enqueue_styles() {
     wp_enqueue_script('jquery', "https://code.jquery.com/jquery-3.3.1.slim.min.js", [], false, true );
     wp_enqueue_script('popper.js', "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js", [], false, true );
     wp_enqueue_script('bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js", [], false, true );
+    wp_enqueue_script('app',  get_template_directory_uri() . '/assets/js/app.js', [], false, true );
 
 }
 
@@ -24,3 +25,169 @@ require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
 
 // Image à la une.
 add_theme_support( 'post-thumbnails' );
+
+
+function register_housing(){
+    register_post_type('housing',[
+        'label' => 'Logements',
+        'labels' => [
+            'name' => 'Logements',
+            'singular_name' => 'Logement',
+            'all_items' => 'Tous les logements',
+            'add_new_item' => 'Ajouter un logement',
+            'edit_item' => 'Éditer le logement',
+            'new_item' => 'Nouveau logement',
+            'view_item' => 'Voir le logement',
+            'search_items' => 'Rechercher parmi les logements',
+            'not_found' => 'Pas de logement trouvé',
+            'not_found_in_trash' => 'Pas de logement dans la corbeille'
+        ],
+        'public' => true,
+        'supports' => ['title', 'editor', 'author', 'thumbnail'],
+        'has_archive' => true,
+        'show_in_rest' => true, // Si on veut activer Gutenberg
+    ]);
+}
+   
+// Ajout des annonces
+add_action( 'init', 'register_housing' );
+
+// ajout des taxonomy
+
+ // type
+ function registerSize(){
+ register_taxonomy('size', 'housing', [
+    'label' => 'Size',
+    'labels' => [
+        'name' => 'Size',
+        'singular_name' => 'Size',
+        'all_items' => 'Tous les size',
+        'edit_item' => 'Éditer le size',
+        'view_item' => 'Voir le size',
+        'update_item' => 'Mettre à jour le size',
+        'add_new_item' => 'Ajouter un size',
+        'new_item_name' => 'Nouveau size',
+        'search_items' => 'Rechercher parmi les size',
+        'popular_items' => 'Size les plus utilisés'
+    ],
+    'hierarchical' => true,
+    'show_in_rest' => true, // Pour Gutenberg
+]);
+}
+    // ajout du type
+add_action( 'init', 'registerSize');
+
+ // VILLE
+ function registerCity(){
+ register_taxonomy('city', 'housing', [
+    'label' => 'Citys',
+    'labels' => [
+        'name' => 'Citys',
+        'singular_name' => 'City',
+        'all_items' => 'Tous les citys',
+        'edit_item' => 'Éditer le city',
+        'view_item' => 'Voir le city',
+        'update_item' => 'Mettre à jour le city',
+        'add_new_item' => 'Ajouter un city',
+        'new_item_name' => 'Nouveau city',
+        'search_items' => 'Rechercher parmi les citys',
+        'popular_items' => 'Citys les plus utilisés'
+    ],
+    'hierarchical' => true,
+    'show_in_rest' => true, // Pour Gutenberg
+]);
+}
+    // ajout du city
+add_action( 'init', 'registerCity' );
+
+
+
+/**
+ * CREATE TABLE `wf3_wordpress`.`wp_contact` ( `id` INT UNSIGNED NOT NULL AUTO_INCREMENT , `reference` VARCHAR(255) NOT NULL , `housing_id` INT NOT NULL , `lastname` VARCHAR(255) NOT NULL , `firstname` VARCHAR(255) NOT NULL , `message` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+ */
+// ce hooke est executé au moment où le back offiche de WP est chargé
+add_action( 'admin_menu', 'contact_menu' );
+
+/** Step 1. */
+function contact_menu() {
+	add_menu_page( 'Demande de contact', 'Demandes de contact', 'manage_options', 'demande-de-contact', 'contact_page' );
+}
+
+/** Step 3. */
+function contact_page() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	} ?>
+	<div class="wrap">
+    <h1>Demandes de contact.</h1>
+
+
+    <style>
+        table {
+        border: medium solid #000000;
+        width: 50%;
+        }
+
+        thead, th, td{
+            text-align : center;
+        }
+
+        th{
+            background-color: lightgrey;
+            border-left : 1px solid black;
+        }
+
+        td{
+            border-left : 1px solid black;
+        }
+    
+
+    </style>
+    
+    
+    <?php 
+    global $wpdb;
+    $contacts =  $wpdb->get_results("SELECT * FROM {$wpdb->prefix}contact");    
+     ?>
+      <hr color="blue"> 
+    <table class="wp-list-table widefat fixed striped">
+        <thead>
+            <tr>
+                 <th scope="col tr">#</th>
+                <th scope="col">Référence  </th>
+                <th scope="col">Annonce  </th>
+                <th scope="col"> Prenom </th>
+                <th scope="col"> Nom </th>
+                <th scope="col"> Message </th>            
+            </tr>
+        </thead>
+    <tbody>
+
+    <?php foreach($contacts as $contact): ?>
+    <tbody>
+        <tr>
+       
+            <th scope="row"> <?= $contact->id ?> </th>
+            <td>  <?= $contact->reference ?> </td>
+
+            <td>  
+                <a target="_blank"href="<?php the_permalink($contact->housing_id) ?>">Voir l'annonce</a>
+               
+            </td>
+            
+            <td>  <?= $contact->lastname ?> </td>
+            <td>  <?= $contact->firstname ?> </td>
+            <td>  <?= $contact->message ?> </td>
+
+            <?php endforeach; ?>
+        </tr>
+        </tbody>
+    </table>
+
+
+
+
+
+
+    </div>
+<?php }
--- index.php
+++ index.php
@@ -1,38 +1,154 @@
-<?php get_header() ?>
+<?php
+
+if ('POST' === $_SERVER['REQUEST_METHOD']){
+    // var_dump($_POST);
+
+    $housing_id = isset($_POST['housing_id']) ? trim(htmlentities($_POST['housing_id'])) : null;
+    $reference = isset($_POST['reference']) ? trim(htmlentities($_POST['reference'])) : null;
+    $lastname = isset($_POST['lastname']) ? trim(htmlentities($_POST['lastname'])) : null;
+    $firstname = isset($_POST['firstname']) ? trim(htmlentities($_POST['firstname'])) : null;
+    $message = isset($_POST['message']) ? trim(htmlentities($_POST['message'])) : null;
+
+
+    $errors = [];
+    
+   if(strlen($reference) <= 0)
+   {
+    $errors['reference'] = "La référence ne doit pas être vide.";
+   } 
+
+   if(strlen($lastname) == 0)
+   {
+    $errors['lastname'] = "Le nom ne doit pas être vide.";
+   } 
+
+   if(strlen($firstname) == 0)
+   {
+    $errors['firstname']= "Le prenom ne doit pas être vide.";
+   } 
+
+   if(strlen($message) < 10)
+   {
+    $errors['message'] = "Le message doit contenir au moins 10 caractères.";
+   } 
+
+   if(empty($errors)){
+      $success = 'Votre demande a été envoyée';
+         // requête SQL pour insérer la demande de contact
+        global $wpdb; // $wpdb->prefix = wp_
+        $wpdb->show_errors();
+        $wpdb->insert($wpdb->prefix.'contact', [
+            'reference'=> $reference,
+            'housing_id' => $housing_id,
+            'lastname'=>$lastname,
+            'firstname'=>$firstname,
+            'message'=> $message
+        ]);
+   }
+}
+
+get_header() ?>
+              
+    <h1 class="text-center my-4">Bienvenue sur <?php bloginfo('name'); ?></h1>
+    <p><?php bloginfo('description'); ?></p>
+
+    <?php //  var_dump($errors ?? null); var_dump($success ?? null);
+    // On affiche toutes les erreurs du formulaire
+        if (!empty($errors)){ ?>
+            <div class="alert alert-danger">
+                <?php foreach ($errors as $field => $error){
+                    echo $field . ':' . $error . '<br />';
+                } ?>
+            </div>
+      <?php } ?>
+
+    <?php if(isset($success)){
+        echo '<div class="alert alert-success">' .$success . '</div>';
+    }?>
+    
+    <div class="row">
       
-
-        
-            <h1 class="text-center my-4">Bienvenue sur <?php bloginfo('name'); ?></h1>
-            <p><?php bloginfo('description'); ?></p>
-      
-
-
-
-
-            <div class="row">
     <?php 
         
         if(have_posts()){ // si on a des articles
             while (have_posts()){  the_post();// On parcourt les carticles ?>
-                <div class="col-3 text-center " id="linkArticle">
+                <div class="col-4 text-center " id="linkArticle">
 
-                <a href="<?php the_permalink(); ?>" class="link"> <h2><?php the_title(); ?> </h2> </a>
-                <?php 
-                    $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' );
-                    $image_url = $large_image_url[0] ?? null;
-                    echo '<img class="img-fluid" src="'.$image_url.'">';
-                 ?>  
-                <a href="<?php the_permalink(); ?>" class="linkgrey">        <p><?= the_excerpt(); ?> </p></a>
-                         <div class="card-footer"><?= get_the_date() ?> </div> 
+                    <a href="<?php the_permalink(); ?>" class="link"> <h2><?php the_title(); ?> </h2> </a>
+                    
+                    <?php 
+                        $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' );
+                        $image_url = $large_image_url[0] ?? null;
+                        echo '<img class="img-fluid" src="'.$image_url.'">';
+                    ?>  
+                    <?php if (!is_home()){ ?>
+                    <div class= "card my-3">
+                        <p>Surface : <?php echo get_post_meta($post->ID, 'surface', true); ?> m² </p> 
+                        <p>Prix : <?php echo get_post_meta($post->ID, 'prix', true); ?> &euro; </p> 
+                        <p class = "terms"><?php echo the_terms($post->ID, 'size', 'Type de logement : '); ?>  </p> 
+                        <p class = "terms"><?php echo the_terms($post->ID, 'city', 'Ville : '); ?>  </p>                    
+                    </div>
+
+
+                    <?php }?>
+
+                    <button data-id="<?php the_ID(); ?>" data-title ="<?php the_title(); ?>" type="button" class="btn btn-primary" data-toggle="modal" data-target="#housing-modal">
+                        Nous contacter
+                    </button>  
+
+                    <a href="<?php the_permalink(); ?>" class="linkgrey">        <p><?= the_excerpt(); ?> </p></a>
+                    <div class="card-footer"><?= get_the_date() ?> </div> 
                      
-
-                 
-                </div>  
-                         
-           <?php }
-        }
+                </div>        
+            </div>            
+           <?php
+        }}
         
     ?>
+    </div>
+
+                      <!-- Modal -->
+    <div class="modal fade" id="housing-modal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Contact</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span >&times;</span>
+                    </button>
+                </div>
+                <form action="" method="post">
+                   
+                    <div class="modal-body">
+
+                        <input type="hidden" name="housing_id" id="housing_id">
+                        <div class="form-group">
+                            <label for="reference">Référence</label>
+                            <input type="text" name="reference" id="reference" class ="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="lastname">Nom</label>
+                            <input type="text" name="lastname" id="lastname" class ="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="firstname">Prénom</label>
+                            <input type="text" name="firstname" id="firstname" class ="form-control">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="message">Message</label>
+                            <input type="text" name="message" id="message" class ="form-control">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                        <button class="btn btn-primary">Envoyer</button>
+                    </div>
+                </form>
+            </div>
         </div>
+    </div> 
 
 <?php get_footer() ?>
